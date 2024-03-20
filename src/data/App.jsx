899aa9ca41b239c669fa12e7f683b1b8f2623a52
micaela prado <pradomicaela99@gmail.com>
2024-03-19 21:23:58 -0500

    invocacion de hijos en el padre app

--- src/data/App.jsx
+++ src/data/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import CartWidget from './components/CartWidget'; 
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <ItemListContainer greeting="Welcome to our store!" />
+      <ItemListContainer greeting="Te damos la bienvenida a Glad!" />
+      <CartWidget />
     </div>
   );
 }
